fa22ee59930140f5996f1f8427e7b9f4bb263d4d
Nikolai Tatianenko <nikolas.tatianenko@gmail.com>
2017-08-09 00:35:30 +0300

    Create Nick Paint Java file

---
+++ NickPaint.java
@@ -0,0 +1,14 @@
+package com.tatianenko.nikolai.java.paint;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class NickPaint {
+
+}
